3758085e6bb357f78a125ea1c0e15f2903bb6f01
Karine <karine.fbueno@gmail.com>
2024-06-16 12:28:39 -0300

    fix: user service e controller

--- backend/app/service/user.service.ts
+++ backend/app/service/user.service.ts
@@ -1,11 +1,31 @@
+import { inject } from '@adonisjs/core'
 import fetch from 'node-fetch'
+import FavoriteService from './favorite.service.js'
 
+@inject()
 export default class UserService {
+  constructor(private favoriteService: FavoriteService) {}
+
   async findUser(username: string) {
     try {
       const apiUrl = `https://api.github.com/users/${username}`
       const apiResponse = await fetch(apiUrl)
-      return await apiResponse.json()
+      const response: any = await apiResponse.json()
+      if (!response.id) {
+        throw new Error('Usuário não encontrado ou sem ID')
+      }
+
+      // Verifique se favoriteService foi inicializado corretamente
+      if (!this.favoriteService || !this.favoriteService.isFavorite) {
+        throw new Error('favoriteService não foi inicializado corretamente')
+      }
+
+      // Verifique se o usuário é favorito
+      const isFavorite = await this.favoriteService.isFavorite(response.id)
+
+      console.log('ID do usuário:', response.id)
+
+      return { status: 'SUCCESSFUL', data: { ...response, isFavorite } }
     } catch (error) {
       console.error('Erro ao buscar dados da API do GitHub:', error)
       throw new Error('Erro ao buscar dados da API do GitHub')
