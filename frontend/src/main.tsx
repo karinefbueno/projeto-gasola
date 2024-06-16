87cebe4170d77df9dbc0143b848479570ffafd2f
Karine <karine.fbueno@gmail.com>
2024-06-16 06:51:38 -0300

    feat: BrowserRouter

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { GlobalStyle } from './styles/globals.ts'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
   <React.StrictMode>
   <GlobalStyle/>
 	<App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </BrowserRouter>
 )
