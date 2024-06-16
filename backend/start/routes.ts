bd15180734c8ea1b4c0be9bcfad77df28171bd86
Karine <karine.fbueno@gmail.com>
2024-06-16 07:15:19 -0300

    feat: rotas e fetch create e delete

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -11,9 +11,11 @@ import router from '@adonisjs/core/services/router'
 
 const ReposController = () => import('#controllers/repos_controller')
 const UsersController = () => import('#controllers/users_controller')
+const FavoriteController = () => import('#controllers/favorites_controller')
 
 router.get('/repos', [ReposController, 'findAll'])
 router.get('/users/:name', [UsersController, 'findUser'])
 
-// router.post('/favotites', [favoriteController, 'findAll'])
-// router.get('/favotites', [favoriteController, 'findAll'])
+router.post('/favorite', [FavoriteController, 'createFavorite'])
+router.get('/favorite', [FavoriteController, 'findAll'])
+router.delete('/favorite/:id', [FavoriteController, 'deleteFavorite'])
