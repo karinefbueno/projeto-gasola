4a3dcffdec045c7d9cc24d64a007095e671e49e1
Karine <karine.fbueno@gmail.com>
2024-06-16 07:12:51 -0300

    fix: ajustes favorite page

---
+++ frontend/src/pages/Favorites/index.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import Header from "../../components/Header";
+import { fetchFavorites } from "../../utils/api";
+import CardRepo from "../../components/CardRepo";
+import { ReposProps } from "../../types/types"; 
+import { ContainerFavoriteCard, ContainerFavorite } from "./style";
+
+function Favorites() {
+  const [favorites, setFavorites] = useState<ReposProps[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchFavorites();
+      const transformedData = data.map((item) => ({
+        id: item.id,
+        owner: {
+          login: item.login,
+          avatar_url: item.avatarUrl,
+          html_url: item.htmlUrl,
+        },
+      }));
+
+      setFavorites(transformedData);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+    <ContainerFavorite>
+      <Header text="Home" href="/" />
+      <ContainerFavoriteCard >
+        {favorites.map((item: ReposProps) => (
+          <CardRepo
+            key={item.id}
+            id={item.id}
+            owner={item.owner}
+          />
+        ))}
+      </ContainerFavoriteCard >
+      </ContainerFavorite>
+    </>
+  );
+}
+
+export default Favorites;
