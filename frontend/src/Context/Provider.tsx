c2bea230056d6f9a03135a47054b5d146edc5e82
Karine <karine.fbueno@gmail.com>
2024-06-19 13:17:47 -0300

    fix: context e provider

--- frontend/src/Context/Provider.tsx
+++ frontend/src/Context/Provider.tsx
@@ -1,16 +1,44 @@
 import { useState } from "react";
 import context from "./Context";
+import { fetchDeleteFavorite, fetchFavorites } from "../utils/api";
+import { ReposProps } from "../types/types";
 
 type ProviderProps = {
   children: React.ReactNode,
 };
 
 function Provider({ children }: ProviderProps) {
-  const [favorited, setFavorited] = useState<boolean>(true);
+  const [favorites, setFavorites] = useState<ReposProps[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const fetchData = async () => {
+    const data = await fetchFavorites();
+    const transformedData = data.map((item) => ({
+      id: item.id,
+      name: item.name,
+      login: item.login,
+      avatar_url: item.avatarUrl,
+      html_url: item.htmlUrl,
+      
+    }));
+    setFavorites(transformedData);
+  };
+
+  const deleteFavorite = async (id: string) => {
+    setLoading(true);
+    const updatedFavorites = favorites
+      .filter((fav) => fav.id !== id.toString());
+    setFavorites(updatedFavorites);
+    setLoading(false);
+
+    await fetchDeleteFavorite(id)
+  }
 
   const contex = {
-    favorited,
-    setFavorited
+    fetchData,
+    favorites,
+    loading,
+    deleteFavorite
   };
 
   return (
