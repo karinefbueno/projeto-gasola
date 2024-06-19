c2503bc90e9812b74314d39c2e5d4350c2be750a
Karine <karine.fbueno@gmail.com>
2024-06-19 13:35:43 -0300

    fix: ajuste estado favorites e tipagem

--- frontend/src/pages/Favorites/index.tsx
+++ frontend/src/pages/Favorites/index.tsx
@@ -1,54 +1,41 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import Header from "../../components/Header";
-import { fetchFavorites } from "../../utils/api";
 import CardRepo from "../../components/CardRepo";
-import { ReposProps } from "../../types/types"; 
-import { ContainerFavoriteCard, ContainerFavorite,Title ,P} from "./style";
+import { ReposProps } from "../../types/types";
+import { ContainerFavoriteCard, ContainerFavorite, Title, P } from "./style";
 import context from "../../Context/Context";
 
 
-
 function Favorites() {
-  const [favorites, setFavorites] = useState<ReposProps[]>([]);
-   const { favorited} = useContext(context);
 
+  const { favorites, fetchData, loading } = useContext(context);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetchFavorites();
-      const transformedData = data.map((item) => ({
-        id: item.id,
-        owner: {
-          login: item.login,
-          avatar_url: item.avatarUrl,
-          html_url: item.htmlUrl,
-        },
-      }));
-
-      setFavorites(transformedData);
-    };
-
     fetchData();
-  }, [favorited]);
+  }, [loading]);
 
   return (
     <>
-    <ContainerFavorite>
-      <Header text="Home" href="/" />
-      <Title>My Favorites</Title>
-      <ContainerFavoriteCard >
-        {favorites.length==0 && <P>No favorite Github users have been added!</P>}
-        {favorites.map((item: ReposProps) => (
-          <CardRepo
-            key={item.id}
-            id={item.id}
-            owner={item.owner}
-          />
-        ))}
-      </ContainerFavoriteCard >
+      <ContainerFavorite>
+        <Header text="Home" href="/" />
+        <Title>My Favorites</Title>
+        <ContainerFavoriteCard >
+          {favorites.length == 0 && <P>No favorite Github users have been added!</P>}
+          {loading ? <P>Loading...</P>
+            : favorites.map((item: ReposProps) => (
+              <CardRepo
+                key={item.id}
+                id={item.id}
+                avatar_url={item.avatar_url}
+                html_url={item.html_url}
+                login={item.login}
+              />
+            ))}
+        </ContainerFavoriteCard >
       </ContainerFavorite>
     </>
   );
 }
 
 export default Favorites;
+
